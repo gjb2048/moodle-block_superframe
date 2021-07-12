54cfee4aa96b6e0ce4968beb58262e9cead4ee0b
Gareth Barnard <1058419+gjb2048@users.noreply.github.com>
2021-07-12 17:26:46 +0100

    Week 3 Task 1.

--- version.php
+++ version.php
@@ -30,6 +30,6 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2021070101;
+$plugin->version   = 2021070102;
 $plugin->requires  = 2018120300; // Minimum Moodle 3.6.
 $plugin->component = 'block_superframe';
